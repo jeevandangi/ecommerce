f9ef2c8164212d57bc41b94705e6aa06e4004ce0
JEEVAN427 <152637779+JEEVAN427@users.noreply.github.com>
2025-03-03 18:50:08 +0545

    url fix

--- client/src/apiResponse/apiResponse.jsx
+++ client/src/apiResponse/apiResponse.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const apiResponseHandler = async (endPoint, method = 'GET', data = null, config) => {
     try {
-        const baseURL = "http://localhost:8000"
+        const baseURL = "https://ecommerce-backend-p3ff.onrender.com"
         const response = await axios({
             url: baseURL + endPoint,
             method: method,
